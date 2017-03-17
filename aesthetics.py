94e62034f6875a83e66f3744145502900586bcda
David Slochower <slochower@gmail.com>
2017-03-17 14:37:05 -0700

    Add standalone python script to generate figures; add docstring to many modules

--- aesthetics.py
+++ aesthetics.py
@@ -1,46 +1,16 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib.colors import colorConverter
 import seaborn as sns
-import numpy as np
-
-def update_label(old_label, exponent_text):
-    if exponent_text == "":
-        return old_label
-    try:
-        units = old_label[old_label.index("(") + 1:old_label.rindex(")")]
-    except ValueError:
-        units = ""
-    label = old_label.replace("({})".format(units), "")
-    exponent_text = exponent_text.replace("$\\times$", "")
-    return "{} ({} {})".format(label, exponent_text, units)
 
 
-def pretty_label(ax, axis='both'):
-    ''' Format the label string with the exponent from the ScalarFormatter '''
-    try:
-        ax.xaxis
-    except:
-        ax = plt.gca()
-
-    ax.ticklabel_format(axis=axis, style='sci')
-    axes_instances = []
-    if axis in ['x', 'both']:
-        axes_instances.append(ax.xaxis)
-    if axis in ['y', 'both']:
-        axes_instances.append(ax.yaxis)
-    for ax in axes_instances:
-        ax.major.formatter._useMathText = True
-        plt.draw()  # Update the text
-        exponent_text = ax.get_offset_text().get_text()
-        label = ax.get_label().get_text()
-        ax.offsetText.set_visible(False)
-        ax.set_label_text(update_label(label, exponent_text))
-
-
-def paper_plot(fig, adjustment=0, scientific=False):
+def prepare_plot():
+    """
+    Set up general plot aesthetics to be used for including figures in a manuscript or talk.
+    These defaults are possibly too large for interactive use.
+    This function should be called before `paper_plot`.
+    """
     sns.set()
     # Increase font size
     sns.set_context("notebook", font_scale=2, rc={"lines.linewidth": 5})
@@ -54,8 +24,19 @@ def paper_plot(fig, adjustment=0, scientific=False):
         r'\renewcommand{\familydefault}{\sfdefault}',
         r'\usepackage[T1]{fontenc}',
         r'\usepackage{graphicx}'
-        ]
+    ]
 
+
+def paper_plot(fig, adjustment=0, scientific=False):
+    """
+    Take a prepared figure and make additional adjustments for inclusion in manuscript,
+    mostly tick thickness, length, and label padding, and include only the left and the bottom
+    axis spines. It would be nice to force the axes to end on a major tick, but I haven't
+    figured out how to do that yet.
+    :param fig:
+    :param adjustment:
+    :param scientific:
+    """
     for ax in fig.axes:
         # Increase padding
         ax.tick_params(which='major', direction='out', length=10, pad=10)
@@ -79,7 +60,7 @@ def paper_plot(fig, adjustment=0, scientific=False):
         alpha = 1
         color_with_alpha = colorConverter.to_rgba(facecolor, alpha)
         fig.patch.set_facecolor(color_with_alpha)
-        # Stick the scientific notation into the axis label, instead of the 
+        # Stick the scientific notation into the axis label, instead of the
         # default position which in the corner, which really makes no sense.
         if ax.xaxis.get_scale() == 'linear':
             if scientific:
@@ -92,21 +73,37 @@ def paper_plot(fig, adjustment=0, scientific=False):
         if adjustment != 0:
             x0, x1, y0, y1 = ax.axis()
             ax.xaxis((x0 - adjustment,
-                    x1 + adjustment,
-                    ))
+                      x1 + adjustment,
+                      ))
         # Make axes thicker
-        for axis in ['top','bottom','left','right']:
+        for axis in ['top', 'bottom', 'left', 'right']:
             ax.spines[axis].set_linewidth(2)
+
 
 def generic_plot(x, y, xlabel=None, ylabel=None, scientific=False, c=None, panel_label=None,
                  panel_xoffset=-0.24, panel_yoffset=0.95):
+    """
+    Quickly plot some data in a consistent style, useful for interactive explorations.
+    :param x: List or array of data
+    :param y: List or array of data
+    :param xlabel: Label for $x$ axis
+    :param ylabel: Label for $y$ axis
+    :param scientific: Scientific notation boolean
+    :param c: Color
+    :param panel_label: Figure label (top left)
+    :param panel_xoffset: Figure label $x$ position fine tuning
+    :param panel_yoffset: Figure label $y$ position fine tuning
+    :return:
+    """
     fig = plt.figure(figsize=(6 * 1.2, 6))
     gs = GridSpec(1, 1, wspace=0.2, hspace=0.5)
     ax = plt.subplot(gs[0, 0])
     if c:
-        ax.plot(x, y, 'o', markersize=10, markeredgecolor='k', markeredgewidth=0.8, alpha=0.5, mfc=c)
+        ax.plot(x, y, 'o', markersize=10, markeredgecolor='k',
+                markeredgewidth=0.8, alpha=0.5, mfc=c)
     else:
-        ax.plot(x, y, 'o', markersize=10, markeredgecolor='k', markeredgewidth=0.8, alpha=0.5, mfc='b')
+        ax.plot(x, y, 'o', markersize=10, markeredgecolor='k',
+                markeredgewidth=0.8, alpha=0.5, mfc='b')
     if xlabel:
         ax.set_xlabel(xlabel)
     if ylabel:
@@ -120,20 +117,60 @@ def generic_plot(x, y, xlabel=None, ylabel=None, scientific=False, c=None, panel
     paper_plot(fig)
     return ax
 
+
+def update_label(old_label, exponent_text):
+    """
+    Update an axis label with scientific notation using LaTeX formatting for the
+    order of magnitude.
+    :param old_label:
+    :param exponent_text:
+    :return:
+    """
+    if exponent_text == "":
+        return old_label
+    try:
+        units = old_label[old_label.index("(") + 1:old_label.rindex(")")]
+    except ValueError:
+        units = ""
+    label = old_label.replace("({})".format(units), "")
+    exponent_text = exponent_text.replace(r"$\times$", "")
+    return "{} ({} {})".format(label, exponent_text, units)
+
+
+def pretty_label(ax, axis='both'):
+    """
+    Format the label string with the exponent from the ScalarFormatter.
+    :param ax:
+    :param axis:
+    """
+    try:
+        ax.xaxis
+    except:
+        ax = plt.gca()
+
+    ax.ticklabel_format(axis=axis, style='sci')
+    axes_instances = []
+    if axis in ['x', 'both']:
+        axes_instances.append(ax.xaxis)
+    if axis in ['y', 'both']:
+        axes_instances.append(ax.yaxis)
+    for ax in axes_instances:
+        ax.major.formatter._useMathText = True
+        plt.draw()  # Update the text
+        exponent_text = ax.get_offset_text().get_text()
+        label = ax.get_label().get_text()
+        ax.offsetText.set_visible(False)
+        ax.set_label_text(update_label(label, exponent_text))
+
+
 def panel_label(panel_label=None,
-                 panel_xoffset=-0.24, panel_yoffset=0.95):
+                panel_xoffset=-0.24, panel_yoffset=0.95):
+    """
+    Add a small label to the top left region of a figure.
+    :param panel_label: Figure label string
+    :param panel_xoffset: Figure label $x$ position fine tuning
+    :param panel_yoffset: Figure label $y$ position fine tuning
+    """
     ax = plt.gca()
     ax.annotate(r'\textbf{{ {} }}'.format(panel_label), xy=(0, 0), xytext=(panel_xoffset, panel_yoffset),
                 xycoords='axes fraction', fontsize=24, fontweight='bold')
-
-#
-# def two_panel_plot(exes, whys, xlabels, ylabels, colors, panel_kws=[-0.24, 0.9]):
-#     fig, axes = plt.subplots(figsize=(2 * 6 * 1.2, 6), nrows=1, ncols=2, gridspec_kw={'wspace':0.2, 'hspace':0.5})
-#     panel_labels = ['a', 'b']
-#     for ax, x, y, xlabel, ylabel, color, panel_label in zip(axes, exes, whys, xlabels, ylabels, colors, panel_labels):
-#         ax.plot(x, y, 'o', markersize=10, markeredgecolor='k', markeredgewidth=0.8, alpha=0.5, mfc=color)
-#         ax.set_xlabel(xlabel)
-#         ax.set_ylabel(ylabel)
-#         ax.annotate(r'\textbf{{ {} }}'.format(panel_label), xy=(0, 0), xytext=(panel_kws[0], panel_kws[1]),
-#                     xycoords='axes fraction', fontsize=24, fontweight='bold')
-#     paper_plot(fig)
--- plot.py
+++ plot.py
@@ -1,13 +1,10 @@
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-from matplotlib.gridspec import GridSpec
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-from matplotlib.colors import colorConverter
-import seaborn as sns
 import numpy as np
-from aesthetics import paper_plot
-from simulation import *
+import seaborn as sns
+from matplotlib.gridspec import GridSpec
 from tqdm import tqdm
+
+from simulation import *
 
 
 def plot_input(this, save=False, filename=None):
@@ -34,7 +31,7 @@ def plot_input(this, save=False, filename=None):
     ax1.plot(range(bins), bound_population, c=bound_clr, ls='--')
     ax1.set_xticks([0, bins / 4, bins / 2, 3 * bins / 4, bins])
     ax1.set_xticklabels(
-        [r'$-\pi$', r'$-\frac{1}{2}\pi{}$', r'$0$', r'$\frac{1}{2}\pi$', r'$\pi$'])
+            [r'$-\pi$', r'$-\frac{1}{2}\pi{}$', r'$0$', r'$\frac{1}{2}\pi$', r'$\pi$'])
     ax1.set_xlabel(r'$\theta$ (rad)')
     ax1.set_ylabel(r'$p$ (input population)')
     paper_plot(fig, scientific=False)
@@ -61,13 +58,13 @@ def plot_energy(this, save=False, filename=None):
     ax1.plot(range(bins), bound_energy, c=bound_clr, ls='--')
     ax1.set_xticks([0, bins / 4, bins / 2, 3 * bins / 4, bins])
     ax1.set_xticklabels(
-        [r'$-\pi$', r'$-\frac{1}{2}\pi{}$', r'$0$', r'$\frac{1}{2}\pi$', r'$\pi$'])
+            [r'$-\pi$', r'$-\frac{1}{2}\pi{}$', r'$0$', r'$\frac{1}{2}\pi$', r'$\pi$'])
     ax1.set_xlabel(r'$\theta$ (rad)')
     ax1.set_ylabel(r'$\mu$ (kcal mol$^{-1}$)')
     paper_plot(fig, scientific=False)
     if save:
         plt.savefig(filename + '.png', dpi=300, bbox_inches='tight')
-    # return fig
+        # return fig
 
 
 def plot_ss(this, save=False, filename=None):
@@ -79,7 +76,7 @@ def plot_ss(this, save=False, filename=None):
 
     bins = this.bins
     unbound_ss = this.ss[0:bins]
-    bound_ss = this.ss[bins:2*bins]
+    bound_ss = this.ss[bins:2 * bins]
     unbound_clr = this.unbound_clr
     bound_clr = this.bound_clr
 
@@ -90,13 +87,13 @@ def plot_ss(this, save=False, filename=None):
     ax1.plot(range(bins), bound_ss, c=bound_clr, ls='--')
     ax1.set_xticks([0, bins / 4, bins / 2, 3 * bins / 4, bins])
     ax1.set_xticklabels(
-        [r'$-\pi$', r'$-\frac{1}{2}\pi{}$', r'$0$', r'$\frac{1}{2}\pi$', r'$\pi$'])
+            [r'$-\pi$', r'$-\frac{1}{2}\pi{}$', r'$0$', r'$\frac{1}{2}\pi$', r'$\pi$'])
     ax1.set_xlabel(r'$\theta$ (rad)')
     ax1.set_ylabel(r'$p$ (probability)')
     paper_plot(fig, scientific=False)
     if save:
         plt.savefig(filename + '.png', dpi=300, bbox_inches='tight')
-    # return fig
+        # return fig
 
 
 def print_parameter(label, value, unit):
@@ -133,9 +130,11 @@ def plot_flux(this, save=False, filename=None):
     print_parameter('k_{cat}', catalytic_rate, 'second**-1')
     print_parameter('[S]', cSubstrate, 'M')
     print_parameter('dt', dt, 'second')
-    print('-'*25)
-    print_parameter('Intrasurface flux', np.mean(unbound_flux + bound_flux), 'cycle second**-1')
-    print_parameter('Peak', np.max(np.hstack((unbound_flux, bound_flux))), 'cycle second**-1')
+    print('-' * 25)
+    print_parameter('Intrasurface flux', np.mean(
+            unbound_flux + bound_flux), 'cycle second**-1')
+    print_parameter('Peak', np.max(
+            np.hstack((unbound_flux, bound_flux))), 'cycle second**-1')
     if load:
         print('-' * 25)
         applied_load = this.load_slope
@@ -147,13 +146,14 @@ def plot_flux(this, save=False, filename=None):
     gs = GridSpec(1, 1, wspace=0.2, hspace=0.5)
     ax1 = plt.subplot(gs[0, 0])
     ax1.plot(range(bins), unbound_flux, c=unbound_clr)
-    ax1.plot(range(bins), bound_flux,   c=bound_clr, ls='--')
-    ax1.plot(range(bins), unbound_flux + bound_flux, 'o', c='k', lw=2, alpha=0.5, zorder=-1, label='Net flux')
+    ax1.plot(range(bins), bound_flux, c=bound_clr, ls='--')
+    ax1.plot(range(bins), unbound_flux + bound_flux, 'o', c='k',
+             lw=2, alpha=0.5, zorder=-1, label='Net flux')
     # ax1.scatter(range(bins), unbound_flux + bound_flux, c='k', marker='+-')
     # ax1.set_xlim([0, bins])
     ax1.set_xticks([0, bins / 4, bins / 2, 3 * bins / 4, bins])
     ax1.set_xticklabels(
-        [r'$-\pi$', r'$-\frac{1}{2}\pi{}$', r'$0$', r'$\frac{1}{2}\pi$', r'$\pi$'])
+            [r'$-\pi$', r'$-\frac{1}{2}\pi{}$', r'$0$', r'$\frac{1}{2}\pi$', r'$\pi$'])
     ax1.set_xlabel(r'$\theta$ (rad)')
     ax1.set_ylabel('Flux $J$ (cycle s$^{-1}$)')
     ax1.legend(frameon=True, loc=1, framealpha=1.0, edgecolor='k')
@@ -185,7 +185,7 @@ def plot_load(this, save=False, filename=None):
 
     ax1.set_xticks([0, bins / 4, bins / 2, 3 * bins / 4, bins])
     ax1.set_xticklabels(
-        [r'$-\pi$', r'$-\frac{1}{2}\pi{}$', r'$0$', r'$\frac{1}{2}\pi$', r'$\pi$'])
+            [r'$-\pi$', r'$-\frac{1}{2}\pi{}$', r'$0$', r'$\frac{1}{2}\pi$', r'$\pi$'])
     ax1.set_xlabel(r'$\theta$ (rad)')
     ax1.set_ylabel(r'$\mu$ (kcal mol$^{-1}$)')
     paper_plot(fig, scientific=False)
@@ -195,6 +195,16 @@ def plot_load(this, save=False, filename=None):
 
 def plot_fluxes_and_velocity(concentrations, directional_flux, reciprocating_flux, velocity,
                              ymin1=None, ymax1=None, label=None):
+    """
+
+    :param concentrations:
+    :param directional_flux:
+    :param reciprocating_flux:
+    :param velocity:
+    :param ymin1:
+    :param ymax1:
+    :param label:
+    """
     cmap = sns.color_palette("Paired", 10)
     fig = plt.figure(figsize=(6 * 1.2, 6))
     gs = GridSpec(1, 1, wspace=0.2, hspace=0.5)
@@ -207,7 +217,8 @@ def plot_fluxes_and_velocity(concentrations, directional_flux, reciprocating_flu
 
     ax2 = ax1.twinx()
     ax2.plot(concentrations, [abs(i) for i in directional_flux], c=cmap[3])
-    ax2.plot(concentrations, [abs(i) for i in reciprocating_flux], c=cmap[3], ls='--')
+    ax2.plot(concentrations, [abs(i)
+                              for i in reciprocating_flux], c=cmap[3], ls='--')
     ax2.set_ylabel('Flux (cycle s$^{{-1}}$)', color=cmap[3])
     ax2.set_ylim([ymin1, ymax1])
     for tl in ax1.get_yticklabels():
@@ -225,7 +236,8 @@ def plot_fluxes_and_velocity(concentrations, directional_flux, reciprocating_flu
         ax.xaxis.labelpad = 15
         ax.yaxis.labelpad = 15
     if label:
-        ax.annotate(r'{}'.format(label), xy=(0.5, 0.5), xytext=(0.18, 0.70), xycoords='figure fraction', fontsize=20)
+        ax.annotate(r'{}'.format(label), xy=(0.5, 0.5), xytext=(
+            0.18, 0.70), xycoords='figure fraction', fontsize=20)
     threshold_labels = ['Directional', 'Reciprocating']
     linestyles = ['-', '--']
     colors = [cmap[3], cmap[3]]
@@ -234,16 +246,31 @@ def plot_fluxes_and_velocity(concentrations, directional_flux, reciprocating_flu
     artists = []
     if threshold_labels:
         for threshold_label, style, c in zip(threshold_labels, linestyles, colors):
-            artists.append(plt.Line2D((0, 1), (0, 0), color=c, linestyle=style))
+            artists.append(plt.Line2D(
+                    (0, 1), (0, 0), color=c, linestyle=style))
         ax.legend([handle for i, handle in enumerate(handles) if i in display] + artists,
-                  [label for i, label in enumerate(labels) if i in display] + threshold_labels,
+                  [label for i, label in enumerate(
+                          labels) if i in display] + threshold_labels,
                   loc='upper left', frameon=True, framealpha=1.0, edgecolor='k')
-    ax1.set_xlim([10**-6, 10**0])
-    ax1.set_xticks([10**-6, 10**-5, 10**-4, 10**-3, 10**-2, 10**-1, 10**0])
+    ax1.set_xlim([10 ** -6, 10 ** 0])
+    ax1.set_xticks([10 ** -6, 10 ** -5, 10 ** -4, 10 ** -3, 10 ** -2, 10 ** -1, 10 ** 0])
     fig.patch.set_facecolor('white')
 
+
 def plot_flux_over_threshold(concentrations, number_above_thresholds, colors, names,
-                             threshold_labels=None, xmin=10**-6, xmax=10**-2, ymin=0, ymax=140):
+                             threshold_labels=None, xmin=10 ** -6, xmax=10 ** -2, ymin=0, ymax=140):
+    """
+
+    :param concentrations:
+    :param number_above_thresholds:
+    :param colors:
+    :param names:
+    :param threshold_labels:
+    :param xmin:
+    :param xmax:
+    :param ymin:
+    :param ymax:
+    """
     fig = plt.figure(figsize=(6 * 1.2, 6))
     gs = GridSpec(1, 1, wspace=0.2, hspace=0.5)
     ax = plt.subplot(gs[0, 0])
@@ -251,21 +278,23 @@ def plot_flux_over_threshold(concentrations, number_above_thresholds, colors, na
     # For simplicity, I think I should enforce paired plotting. That is, to plot the number of angles over two
     # thresholds for each system, with the line styles given above. We should be able to handle an arbitrary number of
     # pairs.
-    pairs = [number_above_thresholds[x:x+2] for x in range(0, len(number_above_thresholds), 2)]
+    pairs = [number_above_thresholds[x:x + 2]
+             for x in range(0, len(number_above_thresholds), 2)]
     for system, color, name in zip(pairs, colors, names):
         ax.plot(concentrations, system[0], ls='-', c=color, label=name)
         ax.plot(concentrations, system[1], ls='--', c=color)
-
 
     handles, labels = ax.get_legend_handles_labels()
     display = (0, 1, 2)
     artists = []
     if threshold_labels:
         for threshold_label, style in zip(threshold_labels, linestyles):
-            artists.append(plt.Line2D((0, 1), (0, 0), color='k', linestyle=style))
+            artists.append(plt.Line2D(
+                    (0, 1), (0, 0), color='k', linestyle=style))
 
         ax.legend([handle for i, handle in enumerate(handles) if i in display] + artists,
-                  [label for i, label in enumerate(labels) if i in display] + threshold_labels,
+                  [label for i, label in enumerate(
+                          labels) if i in display] + threshold_labels,
                   loc='upper left', frameon=True, framealpha=1.0, edgecolor='k')
     ax.set_xlabel('Substrate concentration (M)')
     ax.set_ylabel('Number over threshold')
@@ -274,9 +303,24 @@ def plot_flux_over_threshold(concentrations, number_above_thresholds, colors, na
     ax.set_ylim([ymin, ymax])
     paper_plot(fig)
 
+
 def plot_load_over_threshold(concentrations, number_above_thresholds, colors, names,
                              annotation=None, annotation_x=None, annotation_y=None,
-                             xmin=10**-6, xmax=10**-2, ymin=0, ymax=140):
+                             xmin=10 ** -6, xmax=10 ** -2, ymin=0, ymax=140):
+    """
+
+    :param concentrations:
+    :param number_above_thresholds:
+    :param colors:
+    :param names:
+    :param annotation:
+    :param annotation_x:
+    :param annotation_y:
+    :param xmin:
+    :param xmax:
+    :param ymin:
+    :param ymax:
+    """
     fig = plt.figure(figsize=(6 * 1.2, 6))
     gs = GridSpec(1, 1, wspace=0.2, hspace=0.5)
     ax = plt.subplot(gs[0, 0])
@@ -306,11 +350,11 @@ def return_concentration_slice(df, concentration):
     :param concentration: a target concentration, that will be rounded
     :return: the dataframe slice at the given concentration
     """
-    tmp = df[np.round(df['Concentration'], 1) ==  np.round(concentration, 1)]
+    tmp = df[np.round(df['Concentration'], 1) == np.round(concentration, 1)]
     return tmp
 
 
-def return_fluxes_and_velocity(protein, name, concentrations):
+def return_fluxes_and_velocity(protein, name, concentrations, catalytic_rate=None):
     """
     This helper function will return the turnover rate and the fluxes over a concentration range.
     :param protein: one of the recognized protein systems in the class
@@ -319,29 +363,48 @@ def return_fluxes_and_velocity(protein, name, concentrations):
     :return:
     """
     directional_flux, reciprocating_flux, velocity = [], [], []
-    for concentration in tqdm(concentrations):
+    for concentration in concentrations:
         this = Simulation(data_source=protein)
         this.name = name
         this.cSubstrate = concentration
+        if catalytic_rate:
+            this.catalytic_rate = catalytic_rate
         this.simulate()
         directional_flux.append(np.mean(this.flux_u + this.flux_b))
-        reciprocating_flux.append(np.max(np.hstack((abs(this.flux_u), abs(this.flux_b)))))
-        velocity.append(np.sum(this.ss[this.bins:2*this.bins]) * this.catalytic_rate)
+        reciprocating_flux.append(
+                np.max(np.hstack((abs(this.flux_u), abs(this.flux_b)))))
+        velocity.append(
+                np.sum(this.ss[this.bins:2 * this.bins]) * this.catalytic_rate)
     return directional_flux, reciprocating_flux, velocity
 
 
 def find_above_threshold(df, quantity, threshold):
+    """
+
+    :param df:
+    :param quantity:
+    :param threshold:
+    :return:
+    """
     concentrations = []
     number_above_threshold = []
     for concentration in tqdm(np.unique(df['Concentration'].values)):
         tmp = return_concentration_slice(df, concentration)
-        concentrations.append(10**concentration)
-        number_above_threshold.append(sum(tmp[str(quantity)].abs() > threshold))
+        concentrations.append(10 ** concentration)
+        number_above_threshold.append(
+                sum(tmp[str(quantity)].abs() > threshold))
     return concentrations, number_above_threshold
 
-# To render the dihedrals by their flux values in chimera.
 
 def data_frame_to_chimera(df, df_index_column, df_target_column, filename, chimera_label):
+    """
+
+    :param df:
+    :param df_index_column:
+    :param df_target_column:
+    :param filename:
+    :param chimera_label:
+    """
     file = str(filename) + '.dat'
     f = open(file, 'w')
     f.write('attribute: {}\n'.format(chimera_label))
